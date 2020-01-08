6d9001b0f2bf86278bbb26616a7fca1487b1a03a
fried-man <andrewfriedman01@gmail.com>
2020-01-08 01:22:42 -0500

    Completed L8

---
+++ Module 4/Lab 8/Lab 8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("String: ");
+            Console.WriteLine("String is palindrome: " + palindromeCheck(Console.ReadLine()));
+        }
+
+        static bool palindromeCheck(string input)
+        {
+            if (input.Length == 0)
+            {
+                return true;
+            }
+            else if (input[0] != input[input.Length - 1])
+            {
+                return false;
+            }
+            else
+            {
+                return palindromeCheck(input.Substring(1).Remove(input.Length - 2));
+            }
+        }
+    }
+}
