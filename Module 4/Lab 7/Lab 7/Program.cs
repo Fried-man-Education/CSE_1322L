8c8faed52827d6752a51d575f7ac8a32e6676bf2
fried-man <andrewfriedman01@gmail.com>
2020-01-08 01:33:27 -0500

    Completed L7

---
+++ Module 4/Lab 7/Lab 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("String: ");
+            Console.WriteLine("Amount of vowels: " + vowelCount(Console.ReadLine()));
+        }
+
+        static int vowelCount (string input)
+        {
+            if (input.Length == 0)
+            {
+                return 0;
+            }
+            foreach (char x in "aeiou")
+            {
+                if (input[0] == x)
+                {
+                    return 1 + vowelCount(input.Substring(1));
+                }
+            }
+            return vowelCount(input.Substring(1));
+        }
+    }
+}
