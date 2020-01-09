d21d635780bd82ccc369c35dd23577adf73beef0
fried-man <andrewfriedman01@gmail.com>
2020-01-09 00:19:45 -0500

    Completed L4

---
+++ Module 2/Lab 4/Lab 4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeometricObject.Octagon Shape = new GeometricObject.Octagon(8);
+            Console.WriteLine("Perimeter: " + Math.Round(8d * Shape.side, 2));
+            Console.WriteLine("Area: " + Math.Round((2 + 4 / Math.Sqrt(2)) * Shape.side * Shape.side, 2));
+            GeometricObject.Octagon newShape = Shape.Clone();
+            Console.WriteLine("Objects are same: " + Shape.Compare(newShape));
+        }
+    }
+
+    public abstract class GeometricObject
+    {
+        interface IComparable
+        {
+            bool Compare(Octagon otherObject);
+        }
+        interface ICloneable
+        {
+            Octagon Clone();
+        }
+        public class Octagon : IComparable, ICloneable
+        {
+            public int side;
+            public Octagon(int n)
+            {
+                side = n;
+            }
+            public bool Compare(Octagon otherObject)
+            {
+                if (this.Equals(otherObject))
+                {
+                    return true;
+                }
+                return false;
+            }
+            public Octagon Clone()
+            {
+                return this;
+            }
+        }
+    }
+}
