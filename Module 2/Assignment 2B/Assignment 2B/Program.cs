69ef71801be253831200a3da2c7883a92f7c6eb7
fried-man <andrewfriedman01@gmail.com>
2020-01-09 00:30:07 -0500

    Halfway Completed A2:Source

---
+++ Module 2/Assignment 2B/Assignment 2B/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Assignment_2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create objects?
+        }
+    }
+
+    public abstract class Vacation
+    {
+        float cost, budget;
+        string destination;
+        string KeptToBudget()
+        {
+            if (budget >= cost)
+            {
+                return "Vacation is under " + (budget - cost);
+            }
+            return "Vacation is over" + (cost - budget);
+        }
+
+        public class All_Inclusive : Vacation
+        {
+            string brand;
+            int rating;
+            float totalCost;
+        }
+        public class Piecemeal : Vacation
+        {
+            string[] expenses;
+            float[] costs;
+        }
+    }
+}
