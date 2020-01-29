cc4b994d62ccb53fea28a1ac53fa3a7ccc6ce631
fried-man <andrewfriedman01@gmail.com>
2020-01-29 18:27:12 -0500

    Updated L4

--- Module 2/Lab 4/Lab 4/Program.cs
+++ Module 2/Lab 4/Lab 4/Program.cs
@@ -7,9 +7,9 @@ namespace Lab_4
         static void Main(string[] args)
         {
             GeometricObject.Octagon Shape = new GeometricObject.Octagon(8);
-            Console.WriteLine("Perimeter: " + Math.Round(8d * Shape.side, 2));
-            Console.WriteLine("Area: " + Math.Round((2 + 4 / Math.Sqrt(2)) * Shape.side * Shape.side, 2));
             GeometricObject.Octagon newShape = Shape.Clone();
+            Console.WriteLine("Octagon 1’s side length: " + Shape.side + " and area is: " + Math.Round((2 + 4 / Math.Sqrt(2)) * Shape.side * Shape.side, 2));
+            Console.WriteLine("Octagon 2’s side length: " + newShape.side + " and area is: " + Math.Round((2 + 4 / Math.Sqrt(2)) * newShape.side * newShape.side, 2));
             Console.WriteLine("Objects are same: " + Shape.Compare(newShape));
         }
     }
